59f872fa1f144474f2b8e70ebc539e5aba9f3a9b
Artem Eroshenko <eroshenkoam@qameta.io>
2017-07-19 17:37:19 +0300

    add gradle body

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -7,3 +7,13 @@
 def call(String tasks) {
     sh "./gradlew $tasks"
 }
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    sh "./gradlew ${config.tasks}"
+}
